ee44f11d9d73ed616559140be964033d6d48b616
Piotr Siuszko <siuszko@zoho.com>
2025-09-18 11:02:58 +0200

    Making installer

--- crates/rpack_egui/src/main.rs
+++ crates/rpack_egui/src/main.rs
@@ -13,7 +13,11 @@ fn main() -> eframe::Result<()> {
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([400.0, 300.0])
-            .with_min_inner_size([300.0, 220.0]),
+            .with_icon(
+                eframe::icon_data::from_png_bytes(include_bytes!("../static/base_icon.png"))
+                    .unwrap_or_default(),
+            )
+            .with_min_inner_size([400.0, 300.0]),
         ..Default::default()
     };
     eframe::run_native(
